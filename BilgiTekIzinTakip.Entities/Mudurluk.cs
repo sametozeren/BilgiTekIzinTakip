68f0f58494dc34a159a622754d14862a18593ccb
resrasendur <44396623+resrasendur@users.noreply.github.com>
2019-06-28 15:05:47 +0300

    add entity

---
+++ BilgiTekIzinTakip.Entities/Mudurluk.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgiTekIzinTakip.Entities
+{
+    public class Mudurluk
+    {
+        public String Isim { get; set; }
+
+        public virtual Baskanlik BaskanlikID { get; set; }
+    }
+}
